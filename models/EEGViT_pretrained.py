8ff1961fe1e021227217eeddc806219ce175e588
matthewlkey <matthewlkey@gmail.com>
2023-10-02 23:10:16 -0400

    Update EEGViT_pretrained.py

--- models/EEGViT_pretrained.py
+++ models/EEGViT_pretrained.py
@@ -6,13 +6,13 @@ from torch import nn
 import transformers
 
 class EEGViT_pretrained(nn.Module):
-    def __init__(self, model_name):
+    def __init__(self, model_name, kernel1_y=32, kernel2_x=8, img_size_y=16):
         super().__init__()
         self.conv1 = nn.Conv2d(
             in_channels=1, 
             out_channels=256,
-            kernel_size=(1, 36),
-            stride=(1, 36),
+            kernel_size=(1, kernel1_y),
+            stride=(1, kernel1_y),
             padding=(0,2),
             bias=False
         )
@@ -21,11 +21,11 @@ class EEGViT_pretrained(nn.Module):
 
         config = transformers.ViTConfig.from_pretrained(model_name)
         config.update({'num_channels': 256})
-        config.update({'image_size': (129,14)})
-        config.update({'patch_size': (8,1)})
+        config.update({'image_size': (129,img_size_y)})
+        config.update({'patch_size': (kernel2_x,1)})
 
         model = transformers.ViTForImageClassification.from_pretrained(model_name, config=config, ignore_mismatched_sizes=True)
-        model.vit.embeddings.patch_embeddings.projection = torch.nn.Conv2d(256, 768, kernel_size=(8, 1), stride=(8, 1), padding=(0,0), groups=256)
+        model.vit.embeddings.patch_embeddings.projection = torch.nn.Conv2d(256, 768, kernel_size=(kernel2_x, 1), stride=(kernel2_x, 1), padding=(0,0), groups=256)
         model.classifier=torch.nn.Sequential(torch.nn.Linear(768,1000,bias=True),
                                      torch.nn.Dropout(p=0.1),
                                      torch.nn.Linear(1000,2,bias=True))
